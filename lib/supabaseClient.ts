4bb2e5a1dd2fd2808f612cd1d1508acf039610e3
pben74-droid <pben74@gmail.com>
2026-03-06 17:50:12 +0100

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,18 +1,13 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-console.log("SUPABASE URL:", supabaseUrl)
-console.log("SUPABASE KEY:", supabaseAnonKey?.slice(0,20))
-
 export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey,
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   {
     auth: {
       persistSession: true,
       autoRefreshToken: true,
+      storage: window.localStorage,
       detectSessionInUrl: false
     }
   }
