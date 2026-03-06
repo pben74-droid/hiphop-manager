7cb869a5ab129f7058a035d8e202a820c9e0ff72
pben74-droid <pben74@gmail.com>
2026-03-06 13:27:19 +0100

    Update AuthGuard.tsx

--- components/AuthGuard.tsx
+++ components/AuthGuard.tsx
@@ -16,22 +16,24 @@ export default function AuthGuard({
 
   useEffect(()=>{
 
-    const checkUser = async () => {
+  const checkUser = async () => {
 
-      const { data } = await supabase.auth.getUser()
+    const { data } = await supabase.auth.getUser()
 
-      if(!data.user && pathname !== "/login"){
-  router.push("/login")
-} else {
-  setLoading(false)
-}
-
+    // se NON loggato e NON siamo su login → vai a login
+    if(!data.user && pathname !== "/login"){
+      router.push("/login")
+      return
     }
 
-    checkUser()
+    // se siamo su login oppure loggati → mostra pagina
+    setLoading(false)
 
-  },[])
+  }
 
+  checkUser()
+
+},[pathname])
   if(loading){
     return null
   }
