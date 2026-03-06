98416b134f5a2a8b2c468972c1947c5bce04e79d
pben74-droid <pben74@gmail.com>
2026-03-06 18:50:57 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,3 @@ import { NextResponse } from "next/server"
 export function middleware() {
   return NextResponse.next()
 }
-
-export const config = {
-  matcher: ["/((?!_next|favicon.ico|api).*)"]
-}
