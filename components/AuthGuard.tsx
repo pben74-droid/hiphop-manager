981a48c48c5a952b1540705f7322ea310a28e5ae
pben74-droid <pben74@gmail.com>
2026-03-06 16:35:40 +0100

    Update AuthGuard.tsx

--- components/AuthGuard.tsx
+++ components/AuthGuard.tsx
@@ -1,35 +1,42 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import { useRouter } from "next/navigation"
+import { useRouter, usePathname } from "next/navigation"
 import { supabase } from "@/lib/supabaseClient"
 
-export default function AuthGuard({ children }: any) {
+export default function AuthGuard({
+  children
+}: {
+  children: React.ReactNode
+}) {
 
   const router = useRouter()
-  const [loading, setLoading] = useState(true)
+  const pathname = usePathname()
+  const [loading,setLoading] = useState(true)
 
-  useEffect(() => {
+  useEffect(()=>{
 
-    const checkUser = async () => {
+  const checkUser = async () => {
 
-      const { data } = await supabase.auth.getSession()
+    const { data } = await supabase.auth.getUser()
 
-      if (!data.session) {
-        router.push("/login")
-      } else {
-        setLoading(false)
-      }
-
+    // se NON loggato e NON siamo su login → vai a login
+    if(!data.user && pathname !== "/login"){
+      router.push("/login")
+      return
     }
 
-    checkUser()
+    // se siamo su login oppure loggati → mostra pagina
+    setLoading(false)
 
-  }, [])
-
-  if (loading) {
-    return <div className="p-6">Verifica accesso...</div>
   }
 
-  return children
+  checkUser()
+
+},[pathname])
+  if(loading){
+    return null
+  }
+
+  return <>{children}</>
 }
